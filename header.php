e59ac50b5a0d114bff5fa350a0173b5bd3749235
melanieaxelrod <melanieaxelroddesign@gmail.com>
2017-08-27 00:02:14 -0400

    Menu GOOD
    and homepage also good!! YAY

---
+++ header.php
@@ -0,0 +1,108 @@
+<nav>
+  <div class="navbar">
+    <div class="name">
+      <a href="#" ><div clas="milo">milo axelrod</div></a>
+    </div>
+   
+    <div class="icons">
+      <a href="#" class="nav-icon not-mobile">
+        <img src="/img/icons/profile.svg" />
+        <div class="overlay">
+          <div class="label">profile</div>
+        </div>
+      </a>
+      <a href="#" class="nav-icon not-mobile">
+        <img src="/img/icons/contact.svg" />
+        <div class="overlay">
+          <div class="label">contact</div>
+        </div>
+      </a>
+      <a href="#" class="nav-icon sidebar-toggle-open not-mobile" >
+        <img src="/img/icons/projects.svg" />
+        <div class="overlay">
+          <div class="label">projects</div>
+        </div>
+      </a>
+      <a href="#" class="nav-icon sidebar-toggle-open mobile" >
+        <img src="/img/icons/hamburger.svg" />
+        <div class="overlay">
+          <div class="label">menu</div>
+        </div>
+      </a>
+    </div>
+  </div>
+    
+    <div class="sidebar navbar" >
+      
+    <div class="sidebar-topbit">  
+    <div class="name mobile">
+      <a href="#" ><div clas="milo">milo axelrod</div></a>
+    </div>
+      <div class="icons">
+        <a href="#" class="nav-icon sidebar-toggle-close">
+          <img src="/img/icons/close.svg" />
+          <div class="overlay">
+            <div class="label">close</div>
+          </div>
+        </a>
+      </div>
+      </div>
+      
+      <div class="mobile-nav mobile">
+        <a href="#" class="mobile-link mobile">
+          <div class="mobile-nav-icon"><img src="/img/icons/profile.svg" /></div>
+          <h3>profile</h3>
+          <div class="mobile-nav-icon"><img src="/img/icons/arrow-r.svg" /></div>
+        </a>
+        <a href="#" class="mobile-link mobile">
+          <div class="mobile-nav-icon"><img src="/img/icons/contact.svg" /></div>
+          <h3>contact</h3>
+          <div class="mobile-nav-icon"><img src="/img/icons/arrow-r.svg" /></div>
+        </a>
+        <a href="#" class="mobile-link mobile">
+          <div class="mobile-nav-icon"><img src="/img/icons/projects.svg" /></div>
+          <h3>projects</h3>
+          <div class="mobile-nav-icon"><img src="/img/icons/arrow-d.svg" /></div>
+        </a>
+      </div>
+      
+    <h3 class="not-mobile">Projects</h3>
+    <div class="sidebar-body">
+      <div class="project-link">
+        <div class="project-image">
+          <img src="#" />
+        </div>
+        <div class="project-info">
+        <div class="title">Catskills Conf
+ </div>
+        <div class="type">Branding & Website Design</div>
+          </div>
+      </div>
+      
+      <div class="project-link">
+        <div class="project-image">
+          <img src="#" />
+        </div>
+        <div class="project-info">
+        <div class="title">Midi Jardin
+</div>
+        <div class="type">Restaurant Brand</div>
+        </div>
+      </div>
+      
+      <div class="project-link">
+        <div class="project-image">
+          <img src="#" />
+        </div>
+        <div class="project-info">
+        <div class="title">Muuba
+</div>
+        <div class="type">Mobile App Design</div>
+        </div>
+      </div>
+    </div>
+      
+    </div>
+    
+  </nav>
+  <div class="bottom-line"></div>
